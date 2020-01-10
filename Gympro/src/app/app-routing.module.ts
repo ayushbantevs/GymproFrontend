12c897818eabe0e2195af19ca58f6390303f0cca
Prasadbi <59562139+Prasadbi@users.noreply.github.com>
2020-01-10 16:11:50 +0530

    added gym admin page

--- Gympro/src/app/app-routing.module.ts
+++ Gympro/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GymadminComponent } from './gymadmin/gymadmin.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'gymadmin', component:GymadminComponent},
+  {path:'', component:LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

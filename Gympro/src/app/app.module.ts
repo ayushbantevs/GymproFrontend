e3e3c28004e3b66dbb999c1757554713f1667517
vs-amar <59562003+vs-amar@users.noreply.github.com>
2020-01-10 11:13:14 +0530

    Admin Dashboard Component Added
    App Admin Dashboard Component Added Which is use by app admin Only

--- Gympro/src/app/app.module.ts
+++ Gympro/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminDashBoardComponent } from './admin-dash-board/admin-dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashBoardComponent
   ],
   imports: [
     BrowserModule,

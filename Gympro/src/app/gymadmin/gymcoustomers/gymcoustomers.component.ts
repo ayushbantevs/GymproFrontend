6136de13594198009d6d8e13b0a44a507040aa98
ayushbantevs <59561729+ayushbantevs@users.noreply.github.com>
2020-01-14 19:31:42 +0530

    Theme Change Gymuser
    Changed the theme and Added the Gymuser content and fixed the profile options

--- Gympro/src/app/gymadmin/gymcoustomers/gymcoustomers.component.ts
+++ Gympro/src/app/gymadmin/gymcoustomers/gymcoustomers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GymcoustomersComponent implements OnInit {
 
+  data:any=[{name:'Ayush',joindate:new Date('1/14/2020'),substype:'Monthly',Paidfees:'500',Pending:'0'},{name:'Ayush',joindate:new Date('1/14/2020'),substype:'Monthly',Paidfees:'500',Pending:'0'},{name:'Ayush',joindate:new Date('1/14/2020'),substype:'Monthly',Paidfees:'500',Pending:'0'}];
   constructor() { }
 
   ngOnInit() {

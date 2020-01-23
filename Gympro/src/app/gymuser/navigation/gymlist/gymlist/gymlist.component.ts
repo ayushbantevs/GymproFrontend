a7211f9485b2624919b9c80039a964e418af4878
Pratiknimbalkar <59562771+Pratiknimbalkar@users.noreply.github.com>
2020-01-23 12:50:02 +0530

    Update gymlist.component.ts

--- Gympro/src/app/gymuser/navigation/gymlist/gymlist/gymlist.component.ts
+++ Gympro/src/app/gymuser/navigation/gymlist/gymlist/gymlist.component.ts
@@ -4,24 +4,6 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { Router } from '@angular/router';
 import { Gymlist } from './gymlist';
 
-// export interface UserData  {
-//   id: string;
-//   name: string;
-//   progress: string;
-//   color: string;
-
-
- 
-// }
-// const COLORS: string[] = [
-//   'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
-//   'aqua', 'blue', 'navy', 'black', 'gray'
-// ];
-// const NAMES: string[] = [
-//   'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-//   'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-// ];
-
 
 @Component({
   selector: 'app-gymlist',
